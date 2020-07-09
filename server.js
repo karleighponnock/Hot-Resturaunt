1f64ef86111886190c8887419275f0c34da3365e
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-07-09 14:15:07 -0400

    set up a table variable/array.

--- server.js
+++ server.js
@@ -11,3 +11,12 @@ var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+var tables = [
+    {
+      routeName: "reservedtable",
+      id: "",
+      name: "",
+      email:"",
+      phone:"",
+}
