59e095fa0ea7e16c5e107a06ca272a9918f6a59d
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-07-10 10:26:54 -0400

    Created global variables to table reservations and waitlist.

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 // =============================================================
 var express = require("express");
 var path = require("path");
+var tablesReservation = [];
+var tablesWaitlist = []
+
 
 // Sets up the Express App
 // =============================================================
